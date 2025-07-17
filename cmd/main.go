6cdc45ef5c1345bee80aff8a25857f1a0cf65366
MerrikM <merrikm.pl03@gmail.com>
2025-07-17 11:46:38 +0500

    Update main.go
    - добавлена функция runServer с запуском сервера и graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,76 @@
 package main
 
-func main() {
+import (
+	"MEDODS_TestProject/config/server"
+	"MEDODS_TestProject/internal/handler"
+	"MEDODS_TestProject/internal/repository"
+	"MEDODS_TestProject/internal/security"
+	"context"
+	"github.com/go-chi/chi/v5"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+var secretKey = []byte("SECRET_KEY=4baeef081535397ee96f810e4c205acc7364f1a9cf94b4508d9a")
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	database, err := server.SetupDatabase()
+	if err != nil {
+		log.Fatalf("не удалось подключиться к БД: %v", err)
+	}
+	defer database.Close()
+
+	server, router := server.SetupServer()
+
+	jwtRepository := repository.NewJWTRepository(database)
+	authenticationHandler := handler.NewAuthenticationHandler(jwtRepository)
+
+	router.Route("/api-auth", func(r chi.Router) {
+		r.Group(func(r chi.Router) {
+			r.Use(security.JWTMiddleware(secretKey))
+			r.Get("/me", authenticationHandler.GetCurrentUsersUUID)
+		})
+		r.Group(func(r chi.Router) {
+			r.Get("/get-tokens", authenticationHandler.GetTokens)
+		})
+	})
+
+	runServer(ctx, server)
+}
+
+func runServer(ctx context.Context, server *http.Server) {
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Println("сервер запущен на " + server.Addr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		if err != nil {
+			log.Fatalf("ошибка работы сервера: %v", err)
+		}
+	case sig := <-signalChannel:
+		log.Printf("получен сигнал %v остановки работы сервера ", sig)
+	}
+
+	shutDownCtx, shutDownCancel := context.WithTimeout(ctx, 5*time.Second)
+	defer shutDownCancel()
+
+	if err := server.Shutdown(shutDownCtx); err != nil {
+		log.Printf("ошибка при остановке сервера: %v", err)
+	} else {
+		log.Println("Сервер успешно остановлен")
+	}
 }
